ccdbb0ef3f51e29c114101ddab61a6d7bf2d1995
Vivan Poddar <vivanneil@outlook.com>
2022-04-09 17:12:53 -0400

    dsgdasdgasagds

--- src/main/resources/js/app.js
+++ src/main/resources/js/app.js
@@ -10,4 +10,16 @@ image_input.addEventListener('change', function() {
   reader.readAsDataURL(this.files[0]);
 });
 
+/*
+const image_input2 = document.querySelector("#image_input2");
+var uploaded_image2;
 
+image_input2.addEventListener('change', function() {
+  const reader = new FileReader();
+  reader.addEventListener('load', () => {
+    uploaded_image2 = reader.result;
+    document.querySelector("#display_image2").style.backgroundImage = `url(${uploaded_image})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+});
+*/
